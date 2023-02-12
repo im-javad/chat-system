4100fe4c1db307c72dc7e689458ea4df910d47ab
JavadJadidi <javadjadidi@gmail.com>
2023-02-12 12:25:40 +0330

    communicate-between-tables-were-implemented

--- src/app/Models/Message.php
+++ src/app/Models/Message.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Message extends Model
-{
+class Message extends Model{
     use HasFactory;
+
+    protected $fillable = ['user_id' , 'body'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
